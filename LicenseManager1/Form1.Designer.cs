b22b4a0e77c196fce440ac639682fbcf50640458
bokim <bokim@DESKTOP-Q4ME2IH>
2025-01-19 12:45:16 +0900

    プロジェクト ファイルを追加します。

---
+++ LicenseManager1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace LicenseManager1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLicenseKey = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLicenseKey
+            // 
+            this.txtLicenseKey.Location = new System.Drawing.Point(27, 176);
+            this.txtLicenseKey.Name = "txtLicenseKey";
+            this.txtLicenseKey.Size = new System.Drawing.Size(347, 19);
+            this.txtLicenseKey.TabIndex = 1;
+            this.txtLicenseKey.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(132, 242);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(27, 55);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(161, 97);
+            this.btnValidate.TabIndex = 3;
+            this.btnValidate.Text = "ライセンス確認";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(208, 55);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(166, 97);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "ライセンス生成";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 486);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtLicenseKey);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtLicenseKey;
+        private System.Windows.Forms.Label lblResult;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ LicenseManager1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LicenseManager1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnValidate.Click += new EventHandler(btnValidate_Click);
+            btnGenerate.Click += new EventHandler(btnGenerate_Click);  // 生成ボタンのクリックイベントも追加
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            string licenseKey = txtLicenseKey.Text;
+            if (ValidateLicense(licenseKey))
+            {
+                lblResult.Text = "ライセンスが有効です。";
+                lblResult.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblResult.Text = "ライセンスが無効です。";
+                lblResult.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string newLicenseKey = GenerateRandomLicenseKey();
+            txtLicenseKey.Text = newLicenseKey;
+            lblResult.Text = "新しいライセンスキーが生成されました。";
+            lblResult.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        private bool ValidateLicense(string key)
+        {
+            // ここでライセンスキーの検証ロジックを実装します
+            // 簡単な例として、特定のキーが有効と見なされます
+            return key == "VALID-KEY-1234";
+        }
+
+        private string GenerateRandomLicenseKey()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, 16)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
